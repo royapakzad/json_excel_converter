181144450ab615cdd28fa0ca44c761b1fccc9a87
Roya Pakzad <roya.pakzad@gmail.com>
2020-04-21 09:31:37 -0700

    Update json_excel.py

--- json_excel.py
+++ json_excel.py
@@ -7,14 +7,13 @@ import csv
 # read jason file
 data=pd.read_json(r'input.json')
 # normalize based on different keys
-df = json_normalize(data["needs"])
+df1 = json_normalize(data["needs"])
 df2=json_normalize(data["values"])
 #join keys
-frames = [df, df2]
-result = pd.concat(frames)
-#write to csv
+result = pd.concat([df1,df2])
+#write to csv, all in one sheet
 output_csv=result.to_csv(r'output_csv.csv', index = None, header=True)
 #write to excel, each key in different sheets
 with pd.ExcelWriter('output_excel.xlsx') as writer:  
-    df.to_excel(writer, sheet_name='needs',index = None, header=True)
+    df1.to_excel(writer, sheet_name='needs',index = None, header=True)
     df2.to_excel(writer, sheet_name='values',index = None, header=True)
